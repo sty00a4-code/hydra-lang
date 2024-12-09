bd8dbd69f6555574a54bbbcfda79b5d36d6e318a
sty00a4-code <sty00a4-code@gmail.com>
2024-12-09 17:23:01 +0100

    os module

--- src/std_hydra/std_os.rs
+++ src/std_hydra/std_os.rs
@@ -1,7 +1,16 @@
-use crate::*;
 use crate::run::interpreter::Interpreter;
+use crate::*;
+use std::process;
 
 pub fn import(interpreter: &mut Interpreter) {
     set_global!(interpreter: "os" = make_map!{
+        "id" = native_fn!(_id),
+        "exit" = native_fn!(_exit),
     });
 }
+define_native_fn!(_id (_i args): => {
+    Ok(Some(process::id().into()))
+});
+define_native_fn!(_exit (_i args): code = typed!(args: Int) => {
+    process::exit(code as i32)
+});
